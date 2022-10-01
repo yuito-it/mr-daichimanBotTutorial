bf1a95b129597b4082770c3f924157aed6b381ff
dainy117 <112471785+dainy117@users.noreply.github.com>
2022-10-02 08:44:23 +0900

    Update part3 main.py

--- part3 main.py
+++ part3 main.py
@@ -10,4 +10,4 @@ async def on_ready():
     print("ログインしました！")
     await bot.change_presence(activity=discord.Game(name="1"))
 
-bot.run("MTAyNTY2ODg5Nzg3OTM2NzY5Mg.GMbw0K.0wV8wE0TwuaVqOSFYLx7drgLH7FWA0Rplv8AoY")
+bot.run("token")

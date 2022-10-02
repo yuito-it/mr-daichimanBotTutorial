afaf0a2bce082360c7a9abb32d8439bb4f9dcdfe
dainy117 <112471785+dainy117@users.noreply.github.com>
2022-10-02 19:36:38 +0900

    Create part4 main.py

---
+++ part4 main.py
@@ -0,0 +1,20 @@
+
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='>', intents=intents)
+
+@bot.event
+async def on_ready():
+    print("ログインしました！")
+    await bot.change_presence(activity=discord.Game(name="1"))
+
+@bot.command()
+async def ping(ctx):
+    raw_ping = bot.latency
+    ping = round(raw_ping * 1000)
+    await ctx.reply(f"Pong!\nBotのPing値は{ping}msです。")
+
+bot.run("token")

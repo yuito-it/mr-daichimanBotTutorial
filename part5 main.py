070b06b1869c51834ff7ea8e2c39ead2b7657f34
dainy117 <112471785+dainy117@users.noreply.github.com>
2022-11-13 20:22:51 +0900

    Create part5 main.py

---
+++ part5 main.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext import commands
+
+
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("ボットが起動した！")
+
+@bot.command()
+async def serverinfo(ctx):
+  guild = ctx.message.guild
+  roles =[role for role in guild.roles]
+  text_channels = [text_channels for text_channels in guild.text_channels]
+  embed = discord.Embed(title=f"{guild.name}info",color=0x3683ff)
+  embed.add_field(name="管理者",value=f"{ctx.guild.owner}",inline=False)
+  embed.add_field(name="ID",value=f"{ctx.guild.id}",inline=False)
+  embed.add_field(name="チャンネル数",value=f"{len(text_channels)}",inline=False)
+  embed.add_field(name="ロール数",value=f"{len(roles)}",inline=False)
+  embed.add_field(name="サーバーブースター",value=f"{guild.premium_subscription_count}",inline=False)
+  embed.add_field(name="メンバー数",value=f"{guild.member_count}",inline=False)
+  embed.add_field(name="サーバー設立日",value=f"{guild.created_at}",inline=False)
+  embed.set_footer(text=f"実行者 : {ctx.author} ")
+  await ctx.send(embed=embed)
+
+@bot.command()
+async def userinfo(ctx):
+  embed = discord.Embed(title=f"user {ctx.author.name}",description="userinfo",color=0x3683ff)
+  embed.add_field(name="名前",value=f"{ctx.author.mention}",inline=False)
+  embed.add_field(name="ID",value=f"{ctx.author.id}",inline=False)
+  embed.add_field(name="ACTIVITY",value=f"{ctx.author.activity}",inline=False)
+  embed.add_field(name="TOP_ROLE",value=f"{ctx.author.top_role}",inline=False)
+  embed.add_field(name="discriminator",value=f"#{ctx.author.discriminator}",inline=False)
+  embed.add_field(name="サーバー参加",value=f"{ctx.author.joined_at.strftime('%d.%m.%Y, %H:%M Uhr')}",inline=False)
+  embed.add_field(name="アカウント作成",value=f"{ctx.author.created_at.strftime('%d.%m.%Y, %H:%M Uhr')}",inline=False)
+  embed.set_thumbnail(url=f"{ctx.author.avatar.url}")
+  embed.set_footer(text=f"実行者 : {ctx.author} ")
+  await ctx.send(embed=embed)
+
+bot.run("TOKEN")

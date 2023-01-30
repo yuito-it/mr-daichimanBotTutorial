f40aa9f29626e873a4c0f78ca6371ef3ee2ac61d
dainy117 <112471785+dainy117@users.noreply.github.com>
2023-01-30 20:08:09 +0900

    Update part6 main.py

--- part6 main.py
+++ part6 main.py
@@ -1,1 +1,20 @@
+import discord
+from discord.ext import commands
 
+
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("ボットが起動した！")
+
+@bot.command(pass_content=True)
+@commands.has_permissions(administrator=True)
+async def nick(ctx, member: discord.Member, nick):
+  await member.edit(nick=nick)
+  embed = discord.Embed(title="ニックネームを変更しました",description=f"変更された人物: {member.mention}",color=0xffffff)
+  await ctx.send(embed=embed)
+
+bot.run("TOKEN")
